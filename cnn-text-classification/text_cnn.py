c876aa8492afcf986613519306d0cb2e7df0db2c
Bramble Xu <liangxu006@gmail.com>
2018-06-29 17:27:30 +0900

    write model based on the paper, not finished

---
+++ cnn-text-classification/text_cnn.py
@@ -0,0 +1,7 @@
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Flatten, Input, MaxPooling1D, Convolution1D, Embedding
+from keras.layers.merge import Concatenate
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+def create_model():
