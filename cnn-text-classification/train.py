233e8a9bd84bd1ae3d80e8ddb3efd2613495412a
BrambleXu <liangxu006@gmail.com>
2018-07-01 14:32:55 +0900

    add tensorboard class

--- cnn-text-classification/train.py
+++ cnn-text-classification/train.py
@@ -7,6 +7,47 @@ from word2vec import train_word2vec
 from text_cnn import create_model
 from keras.callbacks import EarlyStopping
 from keras.models import model_from_json
+from keras.callbacks import TensorBoard
+import tensorflow as tf
+
+#========plot train and validation scalars in a same figure=======
+class TrainValTensorBoard(TensorBoard):
+    def __init__(self, log_dir='./logs', **kwargs):
+        # Make the original `TensorBoard` log to a subdirectory 'training'
+        training_log_dir = os.path.join(log_dir, 'training')
+        super(TrainValTensorBoard, self).__init__(training_log_dir, **kwargs)
+
+        # Log the validation metrics to a separate subdirectory
+        self.val_log_dir = os.path.join(log_dir, 'validation')
+
+    def set_model(self, model):
+        # Setup writer for validation metrics
+        self.val_writer = tf.summary.FileWriter(self.val_log_dir)
+        super(TrainValTensorBoard, self).set_model(model)
+
+    def on_epoch_end(self, epoch, logs=None):
+        # Pop the validation logs and handle them separately with
+        # `self.val_writer`. Also rename the keys so that they can
+        # be plotted on the same figure with the training metrics
+        logs = logs or {}
+        val_logs = {k.replace('val_', ''): v for k, v in logs.items() if k.startswith('val_')}
+        for name, value in val_logs.items():
+            summary = tf.Summary()
+            summary_value = summary.value.add()
+            summary_value.simple_value = value.item()
+            summary_value.tag = name
+            self.val_writer.add_summary(summary, epoch)
+        self.val_writer.flush()
+
+        # Pass the remaining logs to `TensorBoard.on_epoch_end`
+        logs = {k: v for k, v in logs.items() if not k.startswith('val_')}
+        super(TrainValTensorBoard, self).on_epoch_end(epoch, logs)
+
+    def on_train_end(self, logs=None):
+        super(TrainValTensorBoard, self).on_train_end(logs)
+        self.val_writer.close()
+
+
 
 #======================== preprocess data ========================
 #
@@ -89,7 +130,9 @@ embedding_layer.set_weights([weights])
 
 # Train model with Early Stopping
 earlystopper = EarlyStopping(monitor='val_loss', patience=5, verbose=1)
-model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, callbacks=[earlystopper],
+tensorboard = TrainValTensorBoard(log_dir='./logs', histogram_freq=0,
+                          write_graph=True, write_images=True)
+model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, callbacks=[earlystopper, tensorboard],
           validation_split=0.1, verbose=2)
 
 # Evaluate
