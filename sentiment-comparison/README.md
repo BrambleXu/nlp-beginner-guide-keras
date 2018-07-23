d66440a43eecc5cd6e571ca53dc675ec37ee1108
Bramble Xu <liangxu006@gmail.com>
2018-07-23 13:08:59 +0900

    add data for model comparison

---
+++ sentiment-comparison/README.md
@@ -0,0 +1,27 @@
+
+Word level CNN
+
+```
+Train on 8635 samples, validate on 960 samples
+Epoch 1/10
+ - 3s - loss: 0.6775 - acc: 0.5709 - val_loss: 0.6175 - val_acc: 0.6661
+Epoch 2/10
+ - 3s - loss: 0.5638 - acc: 0.7108 - val_loss: 0.5505 - val_acc: 0.7068
+Epoch 3/10
+ - 3s - loss: 0.4616 - acc: 0.7808 - val_loss: 0.5008 - val_acc: 0.7396
+Epoch 4/10
+ - 3s - loss: 0.3714 - acc: 0.8360 - val_loss: 0.5000 - val_acc: 0.7552
+Epoch 5/10
+ - 3s - loss: 0.2992 - acc: 0.8748 - val_loss: 0.5381 - val_acc: 0.7719
+Epoch 6/10
+ - 3s - loss: 0.2247 - acc: 0.9111 - val_loss: 0.5335 - val_acc: 0.7729
+Epoch 7/10
+ - 3s - loss: 0.1757 - acc: 0.9302 - val_loss: 0.6405 - val_acc: 0.7630
+Epoch 8/10
+ - 3s - loss: 0.1344 - acc: 0.9506 - val_loss: 0.6503 - val_acc: 0.7755
+Epoch 9/10
+ - 3s - loss: 0.0958 - acc: 0.9624 - val_loss: 0.7699 - val_acc: 0.7688
+Epoch 00009: early stopping
+1067/1067 [==============================] - 0s 86us/step
+test_loss, test_acc: [0.8930901289265516, 0.7535145266545411]
+```
---
+++ sentiment-comparison/word-level/data_helpers.py
@@ -0,0 +1,105 @@
+"""
+In this file: I will implement some data preprocess function
+
+"""
+
+import numpy as np
+import re
+import itertools
+from collections import Counter
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+
+def load_data_and_labels(positive_data_file, negtive_data_file):
+    """
+    Load data from files, split data into words and generate labels
+    Input: the positive data file path and negative data file path
+    Output:
+        x_text: list of words for sentences. e.g [['i', 'am', is'], ['word', 'is', 'too', 'long'], ...,]
+        y: For each sentence, using `[neg, pos]` to represent the lables.
+           - If we have a positive label, we represent it as `[0, 1]`
+           - If we have a negative label, we represent it as `[1, 0]`
+    """
+
+    # Load data from files
+    positive_examples = list(open(positive_data_file, 'r', encoding='utf-8').readlines())
+    positive_examples = [s.strip() for s in positive_examples]
+    negative_examples = list(open(negtive_data_file, 'r', encoding='utf-8').readlines())
+    negative_examples = [s.strip() for s in negative_examples]
+
+    # Split by words
+    x_text = positive_examples + negative_examples
+    x_text = [clean_str(sen) for sen in x_text]
+    x_text = [sen.split(" ") for sen in x_text]
+
+    # Generate labels
+    positive_lables = [[0, 1] for _ in positive_examples]
+    negative_lables = [[1, 0] for _ in negative_examples]
+    y = np.concatenate((positive_lables, negative_lables), 0)
+    return x_text, y
+
+
+def pad_sentences(sentences, padding_word='<PAD/>'):
+    """
+    :param sentences: sentences as list of words,  [['i', 'am', is'], ['word', 'is', 'too', 'long'], ...,]
+    :return: pad sentence to longest length, [['i', 'am', is', '<PAD>', '<PAD>'], ['word', 'is', 'too', 'long', '<PAD>'], ...,]
+    """
+    sequence_length = max(len(sen) for sen in sentences)
+    padded_sentences = []
+    for i in range(len(sentences)):
+        sentence = sentences[i]
+        num_padding = sequence_length - len(sentence)
+        new_sentence = sentence + [padding_word] * num_padding
+        padded_sentences.append(new_sentence)
+    return padded_sentences
+
+
+def build_vocab(sentences):
+    """
+    :param sentences:  sentences after padding
+    :return:
+        vocabulary: a dict object, key is word and value is index. e.g. {'i': 0, 'am':1}
+        vocabulary_inv: a dict object, the inverse of vocabulary. e.g. {0: 'i', 1:'am'}
+    """
+    # Count words
+    word_counts = Counter(itertools.chain(*sentences))
+    # Sort the word as frequency order
+    vocabulay_inv = [x[0] for x in word_counts.most_common()]
+    # Build vocabulary, word: index
+    vocabulay = {word: i for i, word in enumerate(vocabulay_inv)}
+    # Build inverse vocabulary, index: word
+    vocabulay_inv = {value: key for key, value in vocabulay.items()}
+
+    return [vocabulay, vocabulay_inv]
+
+def build_index_sentence(sentences, vocabulary):
+    # x = []
+    # for sen in sentences:
+    #     one_sen = []
+    #     for word in sen:
+    #         one_sen.append(vocabulary[word])
+    #     x.append(one_sen)
+    # return np.array(x)
+
+    # write above code as one line
+    x = np.array([[vocabulary[word] for word in sen] for sen in sentences])
+    return x
+
